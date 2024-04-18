0a7de86d9f5dc1e73153e5341a2a0115d33c60e8
asacco <andres.sacco@travelx.io>
2024-04-18 11:30:42 -0300

    final version

--- api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
+++ api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
@@ -1,14 +1,14 @@
 package com.edteam.reservations.repository;
 
 import com.edteam.reservations.model.Reservation;
+import com.edteam.reservations.model.Status;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -16,4 +16,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Transactional(readOnly = true, timeout = 30)
     List<Reservation> findAll(Specification<Reservation> specification, Pageable pageable);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Reservation e SET e.status = ?1 WHERE e.id = ?2")
+    void updateStatusById(Long id, Status status);
 }
