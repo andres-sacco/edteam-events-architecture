04d7e85819f5f31e71ead6438c79cc16fe0975c3
asacco <andres.sacco@travelx.io>
2024-05-03 10:34:23 -0300

    fix

--- api-reservations/src/test/resources/docker/create_topic.sh
+++ api-reservations/src/test/resources/docker/create_topic.sh
@@ -2,5 +2,5 @@ kafka-topics --create --topic payments --bootstrap-server broker1:29092 --replic
 echo "topic payments was create"
 
 
-kafka-topics --create --topic reservations-payments --bootstrap-server broker1:29092 --replication-factor 3
-echo "topic reservations-payments was create"
+kafka-topics --create --topic reservation-transactions --bootstrap-server broker1:29092 --replication-factor 3
+echo "topic reservation-transactions was create"
